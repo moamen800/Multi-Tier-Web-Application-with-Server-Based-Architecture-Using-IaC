c113e276a7dce978aeffea01e0ac034465ef0131
MoamenAhmed <moamenahmed800@gmail.com>
2024-11-09 17:20:57 +0200

    Initial Commit

---
+++ global_main.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "web_app" {
+  source = "./modules/web_app"
+
+}
+
+module "application_servers" {
+  source = "./modules/application_servers"
+
+}
+
+module "security_groups" {
+  source = "./modules/security_groups"
+
+}
+
